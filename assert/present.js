543e7ef88fc63ea4e31213776ab8c875fdf9ac37
henix <shellpick@gmail.com>
2013-01-06 14:29:28 +0800

    allow a function passed as error message

--- assert/present.js
+++ assert/present.js
@@ -1,5 +1,8 @@
 Assert.present = function(obj, msg) {
 	if ((typeof obj === 'undefined') || (obj === null)) {
+		if (typeof msg == 'function') {
+			msg = msg();
+		}
 		msg = msg ? ': ' + msg : '';
 		Assert.fail('object null or undefined' + msg);
 	}
